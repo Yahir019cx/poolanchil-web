de3adbcea6d56973f83f73336e0c184d2b0526f7
Yahir Sánchez <desarrollo2@mhautomotriz.mx>
2025-10-09 12:25:28 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-primary text-white text-center p-20 text-2xl font-semibold">
+      ✅ Tailwind funcionando en Pool & Chill
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#3CA2A2",
+      },
+    },
+  },
+  plugins: [],
+};
